36bcb7a14059a12cc75725ecd19fbd997af56aaf
Latence132 <alexandre.chaumet@gadz.org>
2018-08-10 17:25:04 +0200

    Parallax page added

---
+++ pages/parallaxtest.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+v-layout(wrap column)
+	v-container.pa-0.ma-0( fluid)
+		v-flex.pa-0.ma-0
+			v-parallax(src="https://cdn.vuetifyjs.com/images/parallax/material.jpg" :height="screenHeight") {{ screenHeight }}
+	v-container.pa-0.ma-0(fluid )
+		v-flex.pa-0.ma-0
+			v-parallax(src="/mer_ecran1.png" :height="screenHeight") {{ screenHeight }}
+	v-container.pa-0.ma-0(fluid )
+		v-flex.pa-0.ma-0
+			v-parallax(src="/nathan-anderson-218687-unsplash.jpg" :height="screenHeight") {{ screenHeight }}
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      screenWindow: 500
+    }
+  },
+  computed: {
+    screenHeight() {
+      return this.screenWindow
+    }
+  },
+  mounted() {
+    console.log( window.innerHeight )
+    this.screenWindow = window.innerHeight
+    this.resizeEvent = () => {
+      console.log( 'resize : ' + window.innerHeight )
+      this.screenWindow = window.innerHeight
+    }
+    window.addEventListener( 'resize', this.resizeEvent )
+  },
+  destroy() {
+    window.removeEventListener( 'resize', this.resizeEvent )
+  }
+}
+</script>
+
+<style lang="stylus">
+</style>
